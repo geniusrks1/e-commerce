ce538a5bb1f5f590cd81a1c22c1a403972c12622
geniusrks1 <gmrohitkumar@gmail.com>
2023-08-03 12:29:29 +0530

    post creating and showing is done

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const ejsMate = require('ejs-mate');
 const path=require('path');
+
 const app=express();
 const PORT=5000;
 
+app.engine('ejs', ejsMate);
 
-app.set('view-engine','ejs');
+
+
+
+app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
 
 app.use(express.static(path.join(__dirname,'public')));
-
-
-
+app.use(express.urlencoded({extended:true}));
 
 mongoose.connect('mongodb://127.0.0.1:27017/e-commerce')
     .then(()=>{console.log('db-connected')})
     .catch(err=> console.log(err));;
+
+
+    const productRoutes=require('./routes/product.js');
+    app.use(productRoutes);
 
 app.get('/',(req,res)=>{
     res.send(`welcome to backend`);
@@ -26,3 +34,21 @@ app.get('/',(req,res)=>{
 app.listen(PORT,(err)=>{
 console.log(`server is running at port ${PORT}`);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
